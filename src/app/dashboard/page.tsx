e790c9917d78aaca7c3a3eeda0ce5299b75b2708
Emmanuel Okpunor <okpunorrex@gmail.com>
2023-08-17 00:50:05 +0100

    dynamic file import for dashboard component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,10 @@
 "use client"
-import Dashboard from "@/components/dashboard/dashboard"
+// import Dashboard from "@/components/dashboard/dashboard"
 import React from "react"
+import dynamic from "next/dynamic"
+const Dashboard = dynamic(() => import("@/components/dashboard/dashboard"), {
+  ssr: false,
+})
 
 type Props = {}
 
